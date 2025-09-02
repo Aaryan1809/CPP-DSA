dd76f68536164e5661e482cb7d865bb7c3f9a18b
Aaryan <aaryanthepiccoder@gmail.com>
2025-09-02 08:33:18 +0530

    tried linked list

--- Linked list/linked_list.cpp
+++ Linked list/linked_list.cpp
@@ -1,19 +1,83 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-class abc
+// means our user defined data type
+// what this node can store so it can store 1 name and address to next node
+class node
 {
+    char name[50];
+    struct node *next;
+    int Count = 0;
+
 public:
-    int a, data = 10;
-};
+    // this will create a node at the begining
+    void atBegin()
+    {
+        node *block = (node *)malloc(sizeof(node));
+        cout << "\nEnter the name : ";
+        cin >> block->name;
+
+        block->next = nullptr;
+
+        if (head != nullptr)
+        {
+            block->next = head;
+            block = head;
+        }
+
+        else
+        {
+            head = block;
+            tail = block;
+        }
+    }
+
+    void atLast()
+    {
+        node *block = (node *)malloc(sizeof(node));
+        cout << "\nEnter the name : ";
+        cin >> block->name;
+
+        block->next = nullptr;
+
+        if (tail != nullptr)
+        {
+            tail->next = block;
+            block = tail;
+        }
+
+        else
+        {
+            head = block;
+            tail = block;
+        }
+    }
+
+    void count()
+    {
+        temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+            Count++;
+        }
+    }
+
+    void display()
+    {
+        count();
+        temp = head;
+        for (int i = 0; i <= Count; i++)
+        {
+            cout << temp->name;
+            temp = temp->next;
+        }
+    }
+} *head, *tail, *temp;
 
 int main()
 {
-    abc a;
-    abc *b = &a;
-    a.data = 199;
-    // b->data = 100;
-    cout << a.data;
-    cout << b->data;
+    node test;
+    test.atBegin();
+    test.atBegin();
     return 0;
 }
--- Search Algos/Binary/basic.cpp
+++ Search Algos/Binary/basic.cpp
@@ -1,112 +1,113 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int bs(vector<int> arr, int tg)
-{
-    int start = 0;
-    int end = arr.size() - 1;
+// int bs(vector<int> arr, int tg)
+// {
+//     int start = 0;
+//     int end = arr.size() - 1;
 
-    while (start <= end)
-    {
-        int mid = (end + start) / 2;
+//     while (start <= end)
+//     {
+//         int mid = (end + start) / 2;
 
-        if (tg < arr[mid])
-        {
-            end = mid - 1;
-        }
+//         if (tg < arr[mid])
+//         {
+//             end = mid - 1;
+//         }
 
-        else if (tg > arr[mid])
-        {
-            start = mid + 1;
-        }
+//         else if (tg > arr[mid])
+//         {
+//             start = mid + 1;
+//         }
 
-        else
-        {
-            return mid;
-        }
-    }
-    return -1;
-}
+//         else
+//         {
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
 
-int bs_desc(vector<int> arr, int tg)
-{
-    int start = 0;
-    int end = arr.size() - 1;
+// int bs_desc(vector<int> arr, int tg)
+// {    
+//     int start = 0;
+//     int end = arr.size() - 1;
 
-    while (start <= end)
-    {
-        int mid = (end + start) / 2;
+//     while (start <= end)
+//     {
+//         int mid = (end + start) / 2;
 
-        if (tg < arr[mid])
-        {
-            start = mid + 1;
-        }
+//         if (tg < arr[mid])
+//         {
+//             start = mid + 1;
+//         }
 
-        else if (tg > arr[mid])
-        {
-            end = mid - 1;
-        }
+//         else if (tg > arr[mid])
+//         {
+//             end = mid - 1;
+//         }
 
-        else
-        {
-            return mid;
-        }
-    }
-    return -1;
-}
+//         else
+//         {
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
 
-int auto_bs(vector<int> arr, int tg)
-{
-    int start = 0;
-    int end = arr.size() - 1;
+// int auto_bs(vector<char> arr, char tg)
+// {
+//     int start = 0;
+//     int end = arr.size() - 1;
 
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-        bool asc = arr[start] < arr[end];
-        if (tg == arr[mid])
-        {
-            return mid;
-        }
+//     while (start <= end)
+//     {
+//         int mid = (start + end) / 2;
+//         bool asc = arr[start] < arr[end];
+//         if (tg == arr[mid])
+//         {
+//             return mid;
+//         }
 
-        if (asc)
-        {
-            if (tg < arr[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                start = mid + 1;
-            }
-        }
+//         if (asc)
+//         {
+//             if (tg < arr[mid])
+//             {
+//                 end = mid - 1;
+//             }
+//             else
+//             {
+//                 start = mid + 1;
+//             }
+//         }
 
-        else
-        {
-            if (tg < arr[mid])
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid - 1;
-            }
-        }
-    }
-}
+//         else
+//         {
+//             if (tg < arr[mid])
+//             {
+//                 start = mid + 1;
+//             }
+//             else
+//             {
+//                 end = mid - 1;
+//             }
+//         }
+//     }
+// }
 
-int main()
-{
-    // ascending order array
-    vector<int> array = {1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
+// int main()
+// {
+//     // ascending order array
+//     vector<char> array = {'a', 's', 'd', 'g', 'f', 'c', 'q', 'e', 'h', 'x', 'f', 'l'};
 
-    // descending order array
-    vector<int> array2 = {9, 8, 7, 6, 5, 5, 4, 3, 2, 1};
+//     // descending order array
+//     vector<char> array2 = {'a', 'c', 'q', 'e', 'h', 'f', 'l', 's', 'd', 'g', 'f', 'c'};
 
-    int target = 2;
-    // cout << bs(array, target);
-    // cout << bs_desc(array2, target);
-    cout << auto_bs(array, target);
-    // cout << auto_bs(array2, target);
-    return 0;
-}
+//     char target = 'x';
+//     // cout << bs(array, target);
+//     // cout << bs_desc(array2, target);
+//     cout << auto_bs(array, target);
+//     // cout << auto_bs(array2, target);
+//     return 0;
+// }
+
